e644289566c662ec9f61310205ccc2f0fd564518
wfh1234 <3470496065@qq.com>
2022-07-19 14:54:40 +0800

    格式修改

--- cp_data.sh
+++ cp_data.sh
@@ -1,6 +1,6 @@
-cp ./config.json /home/jp/zsy/whole_engine_test1/dist/jp_test
-cp ./report_template.xlsx /home/jp/zsy/whole_engine_test1/dist/jp_test
-cp -rf ./shell /home/jp/zsy/whole_engine_test1/dist/jp_test
+cp ./config.json /home/jp/wfh/dist/jp_test
+cp ./report_template.xlsx /home/jp/wfh/dist/jp_test
+cp -rf ./shell /home/jp/wfh/dist/jp_test
 
 cd dist/jp_test/
 mkdir reports
